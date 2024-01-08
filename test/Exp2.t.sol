0c2eed7fd31887509f35fade5a2b4f336c6a2b25
Lawrence Forman <me@merklejerk.com>
2024-01-08 11:55:28 -0500

    ...

--- test/Exp2.t.sol
+++ test/Exp2.t.sol
@@ -16,11 +16,9 @@ enum IncludeStatus {
 
 enum Asset {
     Gold,
-    Apples,
+    Fruit,
     Fish,
     Bread,
-    Milk,
-    Rugs
 }
 
 contract Market {
@@ -38,14 +36,26 @@ contract Market {
     mapping (address => mapping (Asset => uint256)) public balanceOf;
     mapping (Asset asset0 => mapping (Asset asset1 => Pool)) _poolsByAssets;
 
-    modifier onlyActor() {
-        require(msg.sender == GAME.currentActor());
-        _;
-    }
-
     modifier onlyGame() {
         require(msg.sender == address(GAME));
         _;
+    }
+   
+    constructor() {
+        GAME = Game(msg.sender);
+    }
+
+    function createPool(Asset assetA, Asset assetB, uint256 reserveA, uint256 reserveB)
+        external onlyGame
+    {
+        require(assetA != assetB);
+        require(reserveA != 0 && reserveB != 0);
+        (Asset asset0, Asset asset1) = _sortAssetPair(assetA, assetB);
+        Pool memory pool = _poolsByAssets[asset0][asset1];
+        require(pool.reserve0 == 0 && pool.reserve1 == 0);
+        _poolsByAssets[asset0][asset1] = asset0 == assetA
+            ? Pool(reserveA.toUint96(), reserveB.toUint96())
+            : Pool(reserveB.toUint96(), reserveA.toUint96());
     }
 
     function quoteSell(Asset sellAsset, Asset buyAsset, uint256 sellAmount)
@@ -87,9 +97,12 @@ contract Market {
         returns (int128 amountOut0, int128 amountOut1)
     {
         assert(reserve0 > 0 && reserve0 > 0);
+        // TODO: Possible to brick market with bad k if a reserve nears 0?
+        //       Add min liquidity requirement?
         int256 k = int256(reserve0) * int256(reserve1);
         if (amountIn0 != 0) {
             assert(amountIn1 == 0);
+            amountOut0 = amountIn0;
             amountOut1 = amountIn0 < 0
                 ? divUp(k, reserve0 + amountIn0) - reserve1
                 : reserve1 - k / (reserve0 + amountIn0);
@@ -97,15 +110,42 @@ contract Market {
             amountOut0 = amountIn1 < 0
                 ? divUp(k, reserve1 + amountIn1) - reserve0
                 : reserve0 - k / (reserve1 + amountIn1);
+            amountOut1 = amountIn1;
         }
+    }
+
+    function sellForced(Asset sellAsset, Asset buyAsset, uint256 sellAmount)
+        external
+        onlyGame
+        returns (uint256 buyAmount)
+    {
+        buyAmount = _sell(sellAsset, buyAsset, sellAmount);
     }
 
     function sell(Asset sellAsset, Asset buyAsset, uint256 sellAmount)
         external
-        onlyActor
         returns (uint256 buyAmount)
     {
-        // ...
+        buyAmount = _sell(sellAsset, buyAsset, sellAmount);
+        _burn(sellAsset, sellAmount);
+        _mint(buyAsset, buyAmount);
+    }
+
+    function _sell(Asset sellAsset, Asset buyAsset, uint256 sellAmount)
+        private
+        returns (uint256 buyAmount)
+    {
+        (Asset asset0, Asset asset1) = _sortAssetPair(sellAsset, buyAsset);
+        (uint96 reserve0, uint96 reserve1) = _getExistingPool(asset0, asset1);
+        (int128 amount0, int128 amount1) = sellAsset == asset0
+        ? (sellAmount.toInt128(), 0)
+        : (0, sellAmount.toInt128());
+        (amount0, amount1) = _quote(reserve0, reserve1, amount0, amount1);
+        _poolsByAssets[asset0][asset1] = Pool({
+            reserve0: (int128(reserve0) + amount0).toUint96(),
+            reserve1: (int128(reserve1) + amount1).toUint96(),
+        });
+        buyAmount = sellAsset == asset0 ? (-amount1).toUint256() : (-amount0).toUint256();
     }
 
     function _getExistingPool(Asset asset0, Asset asset1)
@@ -126,15 +166,37 @@ contract Market {
         (asset0, asset1) = address(a) > address(b) ? (b, a) : (a, b);
     }
 
+    function mint(Asset asset, address owner, uint256 amount) 
+        external
+        onlyGame
+    {
+        _mint(asset, owner, amount);
+    }
+
     function burn(Asset asset, address owner, uint256 amount)
         external
         onlyGame
     {
+        _burn(asset, owner, amount);
+    }
+
+    function _mint(Asset asset, address owner, uint256 amount) 
+        private
+    {
+        balanceOf[owner][asset] += amount;
+    }
+
+    function _burn(Asset asset, address owner, uint256 amount)
+        private
+    {
         balanceOf[owner][asset] -= amount;
     }
 
-    function transfer(Asset asset, address to, uint256 amount) external onlyActor;
-    function swap(Asset sellAsset, Asset buyAsset, uint256 sellAmount) external onlyActor returns (uint256 buyAmount);
+    function transfer(Asset asset, address to, uint256 amount) external {
+        require(GAME.isPlayer(to));
+        balanceOf[msg.sender][asset] -= amount;
+        balanceOf[to][asset] += amount;
+    }
 }
 
 contract Player {
@@ -196,11 +258,11 @@ function divUp(uint256 n, uint256 d) internal pure returns (uint256 q) {
     return (n + (d - 1)) / d;
 }
 
-// Implements NUM_POOLS AMM curves. Only supports one direction of trades:
+// Implements NUM_PLAYERS AMM curves. Only supports one direction of trades:
 // buying asset0 for asset1. Everytime a buy occurs on a pool, the "bought" asset0 is
 // sold to the other (N-1) pools. This results in the price of asset0 increasing
 // on the pool it was bought from and decreasing on the other pools.
-contract LinkedPools {
+contract BlockMarket {
     using LibSafeCast for uint256;
 
     struct PoolState {
@@ -209,32 +271,31 @@ contract LinkedPools {
         uint128 lastReserve0Accumulator;
     }
     
-    address public immutable OPERATOR;
-    uint32 public immutable NUM_POOLS;
+    address public immutable GAME;
+    uint32 public immutable NUM_PLAYERS;
     uint96 immutable INITIAL_RESERVE0;
     uint96 immutable INITIAL_RESERVE1;
     uint128 _reserve0Accumulator;
     mapping (uint32 poolId => PoolState state) _poolById;
 
-    modifier onlyOperator() {
-        require(msg.sender === OPERATOR);
+    modifier onlyGame() {
+        require(msg.sender == GAME);
         _;
     }
 
     modifier onlyValidPoolId(uint32 poolId) {
-        require(poolId < NUM_POOLS);
+        require(poolId < NUM_PLAYERS);
         _;
     }
 
     constructor(
-        address operator,
+        uint32 numPlayers,
         uint96 initialReserve0,
-        uint96 initialReserve1,
-        uint32 numPools
+        uint96 initialReserve1
     ) {
         require(initialReserve0 * initialReserve1 >= 1e4);
-        OPERATOR = operator;
-        NUM_POOLS = numPools;
+        GAME = msg.sender;
+        NUM_PLAYERS = numPlayers;
         INITIAL_RESERVE0 = initialReserve0;
         INITIAL_RESERVE1 = initialReserve1;
     }
@@ -257,7 +318,7 @@ contract LinkedPools {
             // Sell any accumulated bought amount0s to this pool.
             uint96 amount0 =
                 (reserve0Accumulator - state.lastReserve0Accumulator).toUint96()
-                / NUM_POOLS;
+                / NUM_PLAYERS;
             uint256 k = uint256(state.reserve0) * uint256(state.reserve1);
             state.reserve0 += amount0;
             state.reserve1 = (k / state.reserve0).toUint96();
@@ -297,7 +358,7 @@ contract LinkedPools {
 
     function buy0(uint32 poolId, uint256 amount0)
         external
-        onlyOperator
+        onlyGame
         returns (uint256 amount1)
     {
         (uint96 reserve0, uint96 reserve1) = getPoolState(poolId);
@@ -310,14 +371,14 @@ contract LinkedPools {
         _poolById[poolId] = PoolState({
             reserve0: reserve0,
             reserve1: reserve1,
-            lastReserve0Accumulator: sell0ToAll(amount0_)
+            lastReserve0Accumulator: _sell0ToAll(amount0_)
         });
         return amount1_;
     }
 
     function sell0ToAll(uint256 amount0)
         public
-        onlyOperator
+        onlyGame
     {
         _sell0ToAll(amount0.toUint96());
     }
@@ -327,6 +388,35 @@ contract LinkedPools {
     }
 }
 
+type prng_ptr uint256;
+
+library LibPrng {
+    function create(uint256 seed) internal pure returns (prng_ptr ptr) {
+        assembly("memory-safe") {
+            ptr := mload(0x40)
+            mstore(0x40, add(ptr, 0x20))
+        }
+    }
+
+    function nextUint256(prng_ptr ptr) internal pure returns (uint256 x) {
+        assembly("memory-safe") {
+            mstore(0x00, mload(ptr))
+            x := keccak256(0x00, 0x20)
+            mstore(ptr, x)
+        }
+    }
+
+    function nextUint256(prng_ptr ptr, uint256 modulo) internal pure returns (uint256 x) {
+        x = nextUint256(ptr) % modulo;
+    }
+   
+    function nextBool(prng_ptr ptr) internal pure returns (bool x) {
+        x = nextUint256(ptr) & 0x1 == 1;
+    }
+}
+
+using LibPrng for prng_ptr;
+
 contract Game {
     using LibBytes for bytes;
 
@@ -334,18 +424,22 @@ contract Game {
         uint32 lastActRound;
         bool reverted;
         uint64 gasUsed;
+        uint96 totalGasUsed;
     }
 
     bytes32 constant SUCCESSFUL_BUILD_PREFIX = keccak256('BUILD_SUCCEEDED');
     uint256 constant ONE_BLOCK_UNIT = 1e6;
 
-    LinkedPools immutable BLOCK_MARKET;
-    Market immutable MARKET;
+    BlockMarket public immutable BLOCK_MARKET;
+    Market public immutable MARKET;
+    uint256 public immutable INITIAL_SEED;
 
     mapping (address => ActResult) _lastActResultByPlayer;
-    uint64 _gasUsedByAllPlayersThisRound;
+    mapping (address => )
     uint32 public currentRound; 
     address public currentActor;
+    address public winner;
+    mapping (address => bool) public isPlayer;
     address _currentBuilder;
     address[] _players;
 
@@ -369,7 +463,62 @@ contract Game {
         _;
     }
 
-    function playRound() external onlyGameMaster {
+    constructor(uint256 seed, address[] memory players) {
+        INITIAL_SEED = seed;
+        for (uint256 i; i < players.length; ++i) {
+            _players.push(players[i]);
+            require(!isPlayer[players[i]]);
+            isPlayer[players[i]] = true;
+        }
+        BLOCK_MARKET = new BlockMarket(players.length, 100e18, 1e18);
+        MARKET = new Market();
+        for (uint8 i; i < type(Asset).max + 1; ++i) {
+            for (uint8 j = i + 1; j < type(Asset).max + 1; ++i) {
+                MARKET.createPool(Asset(i), Asset(j), 100e18, 100e18);
+            }
+        }
+    }
+
+    function playRound(address[] memory playersSortedByGas) external onlyGameMaster returns (address winner) {
+        uint256 currentRound_ = currentRound;
+        prng_ptr prng = LibPrng.create(uint256(keccak256(abi.encodePacked(currentRound_, INITIAL_SEED))));
+        _fundPlayers(playersSortedByGas, prng); 
+        _nudgeMarkets(prng);
+        _buildCurrentRoundBlock();
+        currentRound = currentRound_ + 1;
+        return winner;
+    }
+
+    function _fundPlayers(address[] memory playersSortedByGas, prng_ptr prng) private {
+        // Give each player x**y gold, sorted by last round gas usage?
+        // Is that a double punishment for builders because they:
+        //  1. pay gold to win builder status.
+        //  2. receive less gold next round because they might have done some sandwich/arb?
+        //      3. if they do the bare minimum they might still be OK.
+        // If this really is a double punishment then maybr we don't need the personal AMM
+        // for block builder auction.
+        assert(playersSortedByGas.length == _players.length);
+        uint96 lastPlayerGasUsed = type(uint96).max;
+        for (uint256 i; i < playersSortedByGas.length; ++i) {
+            address player = playersSortedByGas[i];
+            assert(isPlayer[player]);
+            uint256 gasUsedByPlayer = _lastActResultByPlayer[player].gasUsed;
+            assert(lastPlayerGasUsed >= gasUsedByPlayer);
+            // TODO prevent duplicates or missing players.
+            uint256 fundAmount = ((i + 1) * 1e4) ** 2 * 1e18 / 1e4; // Maybe linear?
+            player
+        }
+    }
+
+    // What if we don't nudge markets? We just give people money?
+    // No rng at all?
+    function _nudgeMarkets(prng_ptr prng) private {
+        for (uint256 i = 0; i < )
+        (Asset sellAsset, Asset buyAsset) = prng.nextBool()
+            ? (Asset)
+    }
+   
+    function _buildCurrentRoundBlock() private {
         uint256 maxBid;
         uint256 bidWinner;
         address[] memory players = _players;
@@ -387,7 +536,6 @@ contract Game {
             // No builder. Everyone gets in.
             _buildDefaultBlock();
         }
-        ++currentRound;
     }
 
     function _buildDefaultBlock(address[] memory players) private noCurrentBuilder {
@@ -451,7 +599,7 @@ contract Game {
     }
 
     function __buildAndRevert(address builder) external onlySelf {
-        uint256 bid = _callBuild()
+        uint256 bid = _callBuild();
         abi.encode(SUCCESSFUL_BUILD_PREFIX, bid).rawRevert();
     }
 
@@ -469,6 +617,14 @@ contract Game {
         // Must meet minimum block bid for this player.
         require(bid >= BLOCK_MARKET.quote0(player, ONE_BLOCK_UNIT));
         BLOCK_MARKET.buy0(builder, ONE_BLOCK_UNIT);
+        // Note that the builder only needs to have the gold AFTER building the block.
+        // TODO: What if you could pay with ANY asset? Drives asset prices to 1:1
+        //       What if each player had a block discount with a specific asset?
+        //          Akin to staking?
+        //          Same as if they just sold for gold?
+        //       I really don't want every round to be just dumping for gold.
+        //       Maybe any asset BUT gold?
+        //       100 of any non-gold asset?
         MARKET.burn(Asset.Gold, builder, bid);
         _currentBuilder = address(0);
     }
@@ -524,10 +680,30 @@ contract Game {
                 gasUsed := sub(gasUsed, gas())
             }
             result.gasUsed = gasUsed;
+            result.totalGasUsed += gasUsed;
             result.reverted = !succeeded;
         }
+        if (_isPlayerWinner(player)) {
+            if (winner) {
+                // Can replace last winner if arrived more cheaply. 
+                if (result.totalGasUsed < _lastActResultByPlayer[player].totalGasUsed) {
+                    winner = player;
+                    // Only trust last Winner log.
+                    emit Winner(player);
+                }
+            }
+        }
         _lastActResultByPlayer[player] = result;
-        _gasUsedByAllPlayersThisRound += result.gasUsed;
         currentActor = address(0); 
+    }
+
+    function _isPlayerWinner(address player) private view returns (bool) {
+        for (uint256 i = 1; i < type(Asset).max; ++i) {
+            // Player with >= 100 of any asset other than't gold is a winner.
+            if (MARKET.balanceOf(player, Asset(i)) >= 100e18) {
+                return true;
+            }
+        }
+        return false;
     }
 }
