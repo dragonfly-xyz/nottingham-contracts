aa21d52d9d36b91aa3817632526f60e72305e354
Lawrence Forman <me@merklejerk.com>
2024-03-25 23:22:09 -0400

    use AES + RSA encryption for player code do not store keys, just emit them

--- src/contest/Contest.sol
+++ src/contest/Contest.sol
@@ -9,13 +9,24 @@ struct Confirmation {
     uint8 v;
 }
 
+enum SeasonState {
+    Inactive,
+    Started,
+    Closed
+}
+
 struct SeasonInfo {
-    bytes32 pubKey;
-    bytes32 privKey;
+    SeasonState state;
     address winner;
     uint96 unclaimedPrize;
     uint32 playerCodeCount;
     mapping (address player => bytes32 codeHash) playerCodeHashes;
+}
+
+struct EncryptedCodeSubmission {
+    bytes encryptedAesKey;
+    bytes encryptedCode;
+    bytes12 iv;
 }
     
 library LibContest {
@@ -46,22 +57,28 @@ contract Contest {
     error InvalidConfirmationError();
     error NotRegisteredError();
     error NotSeasonError();
-    error SeasonClosedError();
+    error SeasonNotActiveError();
     error SeasonNotClosedError();
     error InvalidKeyError();
     error NotWinnerError();
     error AlreadyClaimedError();
-    error PlayerCodeSizeError();
+    error PlayerSubmissionError();
 
     event Registered(address indexed player);
     event Retired(address indexed player);
-    event SeasonClosed(uint32 indexed season, bytes32 privateKey);
+    event SeasonClosed(uint32 indexed season, bytes privateKey);
     event WinnerDeclared(uint32 indexed season, address indexed winner, uint256 prize);
-    event SeasonStarted(uint32 indexed season, bytes32 publicKey);
+    event SeasonStarted(uint32 indexed season, bytes publicKey);
     event PrizeClaimed(uint32 indexed season, address indexed winner, uint256 prize);
-    event CodeCommitted(uint32 indexed season, address indexed player, bytes32 codeHash, bytes encryptedCode);
+    event CodeCommitted(
+        uint32 indexed season,
+        address indexed player,
+        bytes32 codeHash,
+        EncryptedCodeSubmission submission
+    );
 
-    uint256 MAX_CODE_SIZE = 0x8000;
+    uint256 constant MAX_ENCRYPTED_CODE_SIZE = 0x10100;
+    uint256 constant ENCRYPTED_AES_KEY_SIZE = 128;
 
     address public immutable HOST;
     address public immutable ADMIN;
@@ -117,29 +134,29 @@ contract Contest {
         }
     }
 
-    function isSeasonClosed() external view returns (bool) {
-        return _isSeasonClosed(_seasons[currentSeasonIdx]);
+    function getSeasonState() external view returns (SeasonState) {
+        return _seasons[currentSeasonIdx].state;
     }
 
-    function getSeasonKeys(uint32 seasonIdx)
-        external view returns (bytes32 publicKey, bytes32 privateKey)
-    {
-        return (_seasons[seasonIdx].pubKey, _seasons[seasonIdx].privKey);
-    }
-
-    function setPlayerCode(uint32 seasonIdx, bytes32 codeHash, bytes calldata encryptedCode)
+    function submitCode(
+        uint32 seasonIdx,
+        bytes32 codeHash,
+        EncryptedCodeSubmission calldata submission
+    )
         external
         onlyRegisteredPlayer
         duringSameSeason(seasonIdx)
     {
-        if (encryptedCode.length > MAX_CODE_SIZE) {
-            revert PlayerCodeSizeError();
+        if (submission.encryptedAesKey.length != ENCRYPTED_AES_KEY_SIZE
+            || submission.encryptedCode.length > MAX_ENCRYPTED_CODE_SIZE)
+        {
+            revert PlayerSubmissionError();
         }
         SeasonInfo storage season = _seasons[seasonIdx];
-        if (_isSeasonClosed(season)) revert SeasonClosedError();
+        if (season.state != SeasonState.Started) revert SeasonNotActiveError();
         if (season.playerCodeHashes[msg.sender] == 0) ++season.playerCodeCount;
         season.playerCodeHashes[msg.sender] = codeHash;
-        emit CodeCommitted(seasonIdx, msg.sender, codeHash, encryptedCode);
+        emit CodeCommitted(seasonIdx, msg.sender, codeHash, submission);
     }
 
     function getPlayerCodeHash(uint32 seasonIdx, address player)
@@ -164,42 +181,47 @@ contract Contest {
         return (season.winner, season.unclaimedPrize);
     }
 
-    function newSeason(
-        uint32 prevSeasonIdx,
-        bytes32 seasonPubKey,
+    function startSeason(
+        uint32 seasonIdx,
+        bytes calldata seasonPubKey,
         address prevWinner
     )
         external
         payable
-        duringSameSeason(prevSeasonIdx)
         onlyFrom(HOST)
     {
-        if (seasonPubKey == 0) revert InvalidKeyError();
-        SeasonInfo storage prevSeason = _seasons[prevSeasonIdx];
-        if (!_isSeasonClosed(prevSeason)) revert SeasonNotClosedError();
-        if (prevWinner != address(0)) {
-            uint96 unclaimedPrize_ = unclaimedPrize;
-            uint96 prize = uint96(address(this).balance - msg.value) - unclaimedPrize_;
-            unclaimedPrize = unclaimedPrize_ + prize;
-            prevSeason.unclaimedPrize = prize;
-            prevSeason.winner = prevWinner;
-            emit WinnerDeclared(prevSeasonIdx, prevWinner, prize);
+        if (seasonPubKey.length == 0) revert InvalidKeyError();
+        if (seasonIdx == 0) {
+            // Initial season.
+            if (currentSeasonIdx != 0 || _seasons[0].state != SeasonState.Inactive) {
+                revert NotSeasonError();
+            }
+        } else {
+            SeasonInfo storage prevSeason = _seasons[seasonIdx - 1];
+            if (prevSeason.state != SeasonState.Closed) revert SeasonNotClosedError();
+            if (prevWinner != address(0)) {
+                uint96 unclaimedPrize_ = unclaimedPrize;
+                uint96 prize = uint96(address(this).balance - msg.value) - unclaimedPrize_;
+                unclaimedPrize = unclaimedPrize_ + prize;
+                prevSeason.unclaimedPrize = prize;
+                prevSeason.winner = prevWinner;
+                emit WinnerDeclared(seasonIdx - 1, prevWinner, prize);
+            }
         }
-        uint32 seasonIdx = prevSeasonIdx + 1;
+        _seasons[seasonIdx].state = SeasonState.Started;
         currentSeasonIdx = seasonIdx;
-        _seasons[seasonIdx].pubKey = seasonPubKey;
         emit SeasonStarted(seasonIdx, seasonPubKey);
     }
 
-    function endSeason(uint32 seasonIdx, bytes32 seasonPrivKey)
+    function endSeason(uint32 seasonIdx, bytes calldata seasonPrivKey)
         external
         payable
         onlyFrom(HOST)
     {
-        if (seasonPrivKey == 0) revert InvalidKeyError();
+        if (seasonPrivKey.length == 0) revert InvalidKeyError();
         SeasonInfo storage season = _seasons[seasonIdx];
-        if (_isSeasonClosed(season)) revert SeasonClosedError();
-        season.privKey = seasonPrivKey;
+        if (season.state != SeasonState.Started) revert SeasonNotActiveError();
+        season.state = SeasonState.Closed;
         emit SeasonClosed(seasonIdx, seasonPrivKey);
     }
 
@@ -215,10 +237,6 @@ contract Contest {
     }
 
     receive() payable external {}
-
-    function _isSeasonClosed(SeasonInfo storage season) private view returns (bool) {
-        return season.privKey != bytes32(0);
-    }
 
     function _consumeConfirmation(address registrant, Confirmation memory confirmation) private {
         bytes32 h = LibContest.hashRegistration(
